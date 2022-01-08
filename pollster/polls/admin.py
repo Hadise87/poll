72e29229c83dbf0cc9f6461a8f9ac55545fc1d11
Hadise87 <h.shirudi@gmail.com>
2022-01-08 21:44:48 +0900

    a poll make for programming language

---
+++ pollster/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+
+admin.site.site_header = "Pollster admin"
+admin.site.title = "Pollster admin Area"
+admin.site.index_title = "Welcome to Pollster admin area"
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields':['question_text']}),
+                ('Data Information', {'fields':['pub_date'], 'classes':
+                ['collapse']}), ]
+
+    inlines= [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+
